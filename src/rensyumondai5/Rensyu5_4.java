6e2cbf9b07b2f70bfcdd7c6b49d05fb0548b1c33
git2022ada <h.cpa.park@gmail.com>
2022-05-20 16:22:47 +0900

    変更

--- src/rensyumondai5/Rensyu5_4.java
+++ src/rensyumondai5/Rensyu5_4.java
@@ -4,6 +4,8 @@ public class Rensyu5_4 {
 	
 	public static double calcTriangleArea(double bottom, double height)
 	{
+		double bottom = 10.0;
+		double height = 5.0;
 		return bottom * height /2;
 	}
 	
@@ -13,7 +15,8 @@ public class Rensyu5_4 {
 	}
 	public static void main(String[] args) 
 	{
-		System.out.println("長さ" + calcTriangleArea(10.0, 2.0) + "cm,高さ" + calcTriangleArea(2.0, 5.0) + "cmの三角形の面積は" + calcTriangleArea(10.0, 5.0) + "㎠です");
+		
+		System.out.println("cmの三角形の面積は" + calcTriangleArea(10.0, 5.0) + "㎠です");
 		System.out.println("円の面積は" + calcCirclArea(5.0) + "㎠です");
 		
 	}
